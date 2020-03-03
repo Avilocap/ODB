1d8c80f438db927be35f94b0e8da9c75d2624980
Miguel Ángel Antolín Bermúdez <mruwzum@MacBook-Pro-de-Miguel.local>
2020-03-03 16:30:19 +0100

    dsfas

--- src/main/java/org/springframework/samples/petclinic/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.springframework.samples.petclinic.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.samples.petclinic.model.User;
-
-
-public interface UserRepository extends  CrudRepository<User, String>{
-	
-}
