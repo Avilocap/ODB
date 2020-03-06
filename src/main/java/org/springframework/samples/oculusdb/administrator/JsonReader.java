81e222476265951a54b0b190e64b0c821a127253
Miguel Ángel Antolín <chicoantolin@gmail.com>
2020-03-06 18:30:16 +0100

    json reader path

--- src/main/java/org/springframework/samples/oculusdb/administrator/JsonReader.java
+++ src/main/java/org/springframework/samples/oculusdb/administrator/JsonReader.java
@@ -13,7 +13,7 @@ public class JsonReader {
 	public static void main(String[] args) throws IOException {
 
 		// read json file data to String
-		byte[] jsonData = Files.readAllBytes(Paths.get("/resources/tempFiles/", "applications.json"));
+		byte[] jsonData = Files.readAllBytes(Paths.get("src/main/resources/tempFiles/", "applications.json"));
 
 		// create ObjectMapper instance
 		ObjectMapper objectMapper = new ObjectMapper();
