9d58419da96abcd3fafb13bf153a42e1f6d07cf2
Miguel Ángel Antolín <chicoantolin@gmail.com>
2020-03-08 16:33:08 +0100

    realizado el metodo de lectura de json y creado de aplicaciones a la espera de un dominio que persista para hacer el save en db.

---
+++ src/main/java/org/springframework/samples/oculusdb/administrator/Application2Repository.java
@@ -0,0 +1,19 @@
+package org.springframework.samples.oculusdb.administrator;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.Repository;
+import org.springframework.samples.oculusdb.owner.Pet;
+import org.springframework.samples.oculusdb.owner.PetType;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+public interface Application2Repository extends Repository<Application2, Integer> {
+
+
+   void save(Application2 application2);
+
+
+
+}
--- src/main/java/org/springframework/samples/oculusdb/administrator/JsonReader.java
+++ src/main/java/org/springframework/samples/oculusdb/administrator/JsonReader.java
@@ -1,5 +1,6 @@
 package org.springframework.samples.oculusdb.administrator;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
@@ -7,8 +8,10 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
 public class JsonReader {
+
+	private static Application2Repository applications;
+
 
 	public static void main(String[] args) throws IOException {
 
@@ -17,11 +20,15 @@ public class JsonReader {
 
 		// create ObjectMapper instance
 		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
 
 		// convert json string to object
 		Application2 parsedApp = objectMapper.readValue(jsonData, Application2.class);
 
 		System.out.println("Application Object\n" + parsedApp);
+
+		//applications.save(parsedApp);
+
 
 	}
 
