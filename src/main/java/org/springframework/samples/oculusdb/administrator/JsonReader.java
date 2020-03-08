b28bad53148f58bc7395eafa74b2619ed8b1e516
Miguel Ángel Antolín <chicoantolin@gmail.com>
2020-03-08 13:31:16 +0100

    reader

--- src/main/java/org/springframework/samples/oculusdb/administrator/JsonReader.java
+++ src/main/java/org/springframework/samples/oculusdb/administrator/JsonReader.java
@@ -13,33 +13,16 @@ public class JsonReader {
 	public static void main(String[] args) throws IOException {
 
 		// read json file data to String
-		byte[] jsonData = Files.readAllBytes(Paths.get("src/main/resources/tempFiles/", "applications.json"));
+		byte[] jsonData = Files.readAllBytes(Paths.get("src/main/resources/tempFiles/", "apps.json"));
 
 		// create ObjectMapper instance
 		ObjectMapper objectMapper = new ObjectMapper();
 
 		// convert json string to object
-		Application2 emp = objectMapper.readValue(jsonData, Application2.class);
+		Application2 parsedApp = objectMapper.readValue(jsonData, Application2.class);
 
-		System.out.println("Application Object\n" + emp);
+		System.out.println("Application Object\n" + parsedApp);
 
-		// convert Object to json string
-		Application2 application1 = createApplication();
-		// configure Object mapper for pretty print
-		objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
-
-		// writing to console, can write to any output stream such as file
-		StringWriter stringApp = new StringWriter();
-		objectMapper.writeValue(stringApp, application1);
-		System.out.println("App JSON is\n" + stringApp);
-	}
-
-	public static Application2 createApplication() {
-
-		Application2 application = new Application2();
-		application.setName("David");
-
-		return application;
 	}
 
 }
