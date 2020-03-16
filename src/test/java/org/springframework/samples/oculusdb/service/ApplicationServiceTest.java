66ddff93026b93d3e4ce59dd924e537ba41d2dac
Miguel Ángel Antolín <chicoantolin@gmail.com>
2020-03-16 19:08:58 +0100

    cambiado application properties para adptarse a los tetes y añadidos mas tests sobre los servicios existentes

--- src/test/java/org/springframework/samples/oculusdb/service/ApplicationServiceTest.java
+++ src/test/java/org/springframework/samples/oculusdb/service/ApplicationServiceTest.java
@@ -2,15 +2,23 @@
 package org.springframework.samples.oculusdb.service;
 
 import java.security.Provider.Service;
+import java.util.Collection;
+import java.util.HashSet;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.samples.oculusdb.OculusDBApplication;
 import org.springframework.samples.oculusdb.controllers.ApplicationService;
+import org.springframework.samples.oculusdb.model.Application;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
+@SpringBootTest
 public class ApplicationServiceTest {
 
 	@Autowired
@@ -22,4 +30,10 @@ public class ApplicationServiceTest {
 		Assertions.assertEquals(count, 3);
 	}
 
+
+	@Test
+	public void testCountWithInitialData2() {
+		Collection<Application> applications = new HashSet<Application>((Collection<? extends Application>) this.applicationService.findAll());
+		Assertions.assertNotNull(applications);
+	}
 }
