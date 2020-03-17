4cf3be7a9941adb47eca193afaadd2f8911d2705
adria <adria@DESKTOP-GUMRH7M.home>
2020-03-17 19:20:07 +0100

    datos sobre aplicacion

--- src/main/java/org/springframework/samples/oculusdb/controllers/ApplicationController.java
+++ src/main/java/org/springframework/samples/oculusdb/controllers/ApplicationController.java
@@ -4,10 +4,12 @@ package org.springframework.samples.oculusdb.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.oculusdb.model.Application;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -26,13 +28,19 @@ public class ApplicationController {
 		
 	}
 	
+	@GetMapping("/application/{applicationId}")
+	public String application(@RequestParam("applicationId") int applicationId, ModelMap model) {
+		model.put("applications", this.applicationService.findApplicationById(applicationId));
+		return "applications/applicationsDetails";
+	}
+	
 
 	
 	@GetMapping("/applications/{applicationId}")
-	public ModelAndView showApplication(@PathVariable("applicationId") int applicationId) {
+	public ModelAndView showOwner(@PathVariable("applicationId") int applicationId) {
+		
 		ModelAndView mav = new ModelAndView("applications/applicationsDetails");
 		mav.addObject(this.applicationService.findApplicationById(applicationId));
 		return mav;
 	}
-
 }
