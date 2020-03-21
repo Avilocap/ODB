986ab6cc2a33d797c1c2efc87f2a55158c686887
josemagb23 <jmgallardob98@gmail.com>
2020-03-21 22:47:55 +0100

    Vista (corregir y probar)

--- src/main/java/org/springframework/samples/oculusdb/repositories/UserRepository.java
+++ src/main/java/org/springframework/samples/oculusdb/repositories/UserRepository.java
@@ -1,8 +1,12 @@
+
 package org.springframework.samples.oculusdb.repositories;
+
+import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.samples.oculusdb.model.User;
 
 public interface UserRepository extends CrudRepository<User, Integer> {
 
+	Optional<User> findByUsername(String username);
 }
