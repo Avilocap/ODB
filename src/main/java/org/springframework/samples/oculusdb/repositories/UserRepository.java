f02a0fce0c37de8caea2009c31bee9b1949cf47c
Manu <manuel_sfc98@hotmail.com>
2020-03-23 13:28:59 +0100

    login class not working

--- src/main/java/org/springframework/samples/oculusdb/repositories/UserRepository.java
+++ src/main/java/org/springframework/samples/oculusdb/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package org.springframework.samples.oculusdb.repositories;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.samples.oculusdb.model.User;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
+	@Query("select u from User u where u.userAccount.nick = ?1")
 	Optional<User> findByNick(String nick);
+
 }
