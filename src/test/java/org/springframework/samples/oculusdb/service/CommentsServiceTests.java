301e7111304de51e2d00d0e3b65804b5c7dfafee
Manu <manuel_sfc98@hotmail.com>
2020-03-30 14:19:47 +0200

    new tests in CommentsServiceTests

--- src/test/java/org/springframework/samples/oculusdb/service/CommentsServiceTests.java
+++ src/test/java/org/springframework/samples/oculusdb/service/CommentsServiceTests.java
@@ -10,7 +10,6 @@ import org.springframework.samples.oculusdb.model.Application;
 import org.springframework.samples.oculusdb.services.ApplicationService;
 import org.springframework.samples.oculusdb.services.CommentsService;
 
-import javax.validation.Validator;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -72,13 +71,33 @@ public class CommentsServiceTests {
 	}
 
 	@Test
+	void AddCommentWithRandomAppId(){
+		Application app2 = new Application();
+		Optional<Application> ap2 = this.applicationService.findApplicationById(276);
+		if (ap2.isPresent()) {
+			app2 = ap2.get();
+			Comments comment = new Comments();
+			app2.addComment(comment);
+			comment.setTitle("jajaaj");
+			comment.setContent("this is a new comment");
+			this.commentsService.saveComment(comment);
+		} else {
+			app2 = null;
+		}
+		Assertions.assertNull(app2);
+	}
+
+	@Test
 	void shouldFindCommentByAppId() {
 		Collection<Comments> comments = (Collection<Comments>) this.commentsService.findAllByAplicationId(3);
 		Assertions.assertEquals(1, comments.size());
 
-		comments = (Collection<Comments>) this.commentsService.findAllByAplicationId(00);
-		Assertions.assertEquals(0,comments.size());
+	}
 
+	@Test
+	void FindCommentByRandomAppId(){
+		Collection<Comments> comments = (Collection<Comments>) this.commentsService.findAllByAplicationId(276);
+		Assertions.assertEquals(0,comments.size());
 	}
 
 
