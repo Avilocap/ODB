ceb50a2f77c31679d92d9a534287a4012ebbf32d
adrmadfer <adrmadfer@alum.us.es>
2020-04-14 15:04:18 +0200

    some unit tests

--- src/test/java/org/springframework/samples/oculusdb/service/CommentsServiceTests.java
+++ src/test/java/org/springframework/samples/oculusdb/service/CommentsServiceTests.java
@@ -9,6 +9,7 @@ import org.springframework.samples.oculusdb.application.Comments;
 import org.springframework.samples.oculusdb.model.Application;
 import org.springframework.samples.oculusdb.services.ApplicationService;
 import org.springframework.samples.oculusdb.services.CommentsService;
+import org.w3c.dom.Comment;
 
 import java.util.Collection;
 import java.util.Optional;
@@ -101,4 +102,44 @@ public class CommentsServiceTests {
 	}
 	// TODO Añadir casos negativos
 
+	@Test
+	public void findCommentByAppNegative(){
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Collection<Comments> comments = (Collection<Comments>) this.commentsService.findAllByAplicationId(38282383);
+			Assertions.assertNotNull(comments);
+
+		});
+	}
+
+	@Test
+	public void shouldFindCommentById(){
+		Optional<Comments> comment =  this.commentsService.findCommentById(1001);
+		Assertions.assertNotNull(comment);
+	}
+
+
+	@Test
+	public void shouldFindCommentByIdNegative(){
+		Assertions.assertThrows(NullPointerException.class, () -> {
+			Optional<Comments> comment = this.commentsService.findCommentById(100001);
+			Assertions.assertNotNull(comment);
+		});
+	}
+/*
+	@Test
+	void shouldByDeleteComment() {
+		Collection<Comments> comments = (Collection<Comments>) this.commentsService.findAllByAplicationId(101);
+		Optional<Comments> comment = this.commentsService.findCommentById(101);
+
+		Comments comment1 = new Comments();
+		if (comment.isPresent()) {
+			comment1= comment.get();
+		}
+		this.commentsService.deleteComment(comment1);
+
+		Collection<Comments> comments2 = (Collection<Comments>) this.commentsService.findAllByAplicationId(101);
+		Assertions.assertEquals(comments.size()-1, comments2.size());
+	}
+
+ */
 }
