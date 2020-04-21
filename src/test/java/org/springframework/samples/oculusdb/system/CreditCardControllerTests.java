153609da1ad27f67773d298f6c1a1b21f54f63ba
josemagb23 <jmgallardob98@gmail.com>
2020-04-21 14:08:31 +0200

    Some ApplicationControllersIntegrationTests

---
+++ src/test/java/org/springframework/samples/oculusdb/system/CreditCardControllerTests.java
@@ -0,0 +1,44 @@
+package org.springframework.samples.oculusdb.system;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.samples.oculusdb.controllers.CreditCardController;
+import org.springframework.samples.oculusdb.services.CreditCardService;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class CreditCardControllerTests {
+
+    @Autowired
+    private CreditCardController creditCardController;
+
+    @MockBean
+    private CreditCardService creditCardService;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @WithMockUser("testuser")
+    @Test
+    void testInitCreditCardForm() throws Exception {
+        mockMvc.perform(get("/creditCard/new")).andExpect(status().isOk())
+                .andExpect(view().name("creditCard/creditCardForm.html"));
+    }
+
+    @WithMockUser("testuser")
+    @Test
+    void testInitCreditCardFormHasErrors() throws Exception {
+        mockMvc.perform(get("/creditCard/neww")).andExpect(status().is4xxClientError());
+    }
+
+}
