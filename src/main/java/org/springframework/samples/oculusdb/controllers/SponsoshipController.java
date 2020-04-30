341dcf2d635b8069ffff01d8023cffc34c7d334e
Manu <manuel_sfc98@hotmail.com>
2020-04-30 18:18:59 +0200

    fix an error

--- src/main/java/org/springframework/samples/oculusdb/controllers/SponsoshipController.java
+++ src/main/java/org/springframework/samples/oculusdb/controllers/SponsoshipController.java
@@ -2,10 +2,7 @@
 package org.springframework.samples.oculusdb.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.oculusdb.application.Comments;
-import org.springframework.samples.oculusdb.model.Application;
 import org.springframework.samples.oculusdb.model.User;
-import org.springframework.samples.oculusdb.services.CreditCardService;
 import org.springframework.samples.oculusdb.services.SponsorService;
 import org.springframework.samples.oculusdb.services.SponsorShipService;
 import org.springframework.samples.oculusdb.services.UserService;
@@ -14,14 +11,9 @@ import org.springframework.samples.oculusdb.sponsor.Sponsorship;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/sponsorship")
