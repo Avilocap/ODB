9beaa3669e26991c11b64c57651347a4a9bc33da
Manu <manuel_sfc98@hotmail.com>
2020-05-25 16:38:49 +0200

    Some UI Tests added

--- src/test/java/org/springframework/samples/oculusdb/ui/LoginUITest.java
+++ src/test/java/org/springframework/samples/oculusdb/ui/LoginUITest.java
@@ -1,23 +1,18 @@
 package org.springframework.samples.oculusdb.ui;
 
-import java.util.regex.Pattern;
-import java.util.concurrent.TimeUnit;
-import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
-
 import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.transaction.Transactional;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.fail;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Disabled
@@ -33,8 +28,8 @@ public class LoginUITest {
 
 	@BeforeEach
 	public void setUp() throws Exception {
-		System.setProperty("webdriver.gecko.driver", "C:\\Users\\CimaGrande\\Documents\\geckodriver.exe");
-		driver = new FirefoxDriver();
+		System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
+		driver = new ChromeDriver();
 		baseUrl = "https://www.google.com/";
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
